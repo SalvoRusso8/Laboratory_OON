fe74d52b1168f963f9408f9c9ecc75b777ca3f56
Salvo <s267560@studenti.polito.it>
2020-10-14 18:55:26 +0200

    final version of first lab

--- exercise_1.py
+++ exercise_1.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def my_sum(a, b):
     return a + b
 
@@ -8,3 +12,20 @@ def my_division(a, b):
 
 def my_power(base, exponent):
     return base ** exponent
+
+
+def my_sin(n_points, arr):
+    x = np.array([])
+    y = np.array([i**2 for i in range(n_points)])
+    x1 = np.ones(y.shape)
+
+    plt.figure()
+    plt.plot(arr, x1, 'or', label='example')
+    plt.grid()
+    plt.legend()
+    plt.xlabel('xlab')
+    plt.ylabel('ylab')
+
+    plt.show()
+
+    return np.sin(arr)
