183955df2ff5d149df0370bdaeab0c6ac3cd6b2f
Salvo <s267560@studenti.polito.it>
2020-12-05 15:38:08 +0100

    Implemented class Line
    lab 3.3

--- lab3/elements.py
+++ lab3/elements.py
@@ -1,4 +1,5 @@
 from lab3.info import Signal_information
+from scipy.constants import c
 
 class Node(object):
     def __init__(self, node_dictionary):
@@ -35,3 +36,45 @@ class Node(object):
             signal_information.next()
             signal_information = line.propagate(signal_information)
         return signal_information
+
+
+class Line(object):
+    def __init__(self, line_dictionary):
+        self._label = line_dictionary['label']
+        self._length = line_dictionary['length']
+        self._successive = {}
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def successive(self):
+        return self._successive
+
+    @successive.setter
+    def successive(self, successive):
+        self._successive = successive
+
+    def latency_generation(self):
+        latency = self.length / (c * 2 / 3)
+        return latency
+
+    def noise_generation(self, signal_power):
+        noise = 1e-9 * signal_power * self.length
+        return noise
+
+    def propagate(self, signal_information):
+        signal_power = signal_information.signal_power
+        noise = self.noise_generation(signal_power)
+        signal_information.add_noise(noise)
+        latency = self.latency_generation()
+        signal_information.add_latency(latency)
+        node = self.successive[signal_information.path[0]]
+        signal_information = node.propagate(signal_information)
+        return signal_information
+
