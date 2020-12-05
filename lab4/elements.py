1a999cef1efef1676d6b8ad37e0c65b7efd08aeb
Salvo <s267560@studenti.polito.it>
2020-12-05 17:14:49 +0100

    Implemented class Connection
    lab 4.4

--- lab4/elements.py
+++ lab4/elements.py
@@ -188,6 +188,43 @@ class Network(object):
         my_df_filtered = my_df[(my_df['path'].str[0] == input_node) & (my_df['path'].str[-1] == output_node)]
         return my_df_filtered.values[0]
 
+    
+class Connection(object):
+    def __init__(self, input, output, signal_power):
+        self._input = input
+        self._output = output
+        self._signal_power = signal_power
+        self._latency = 0.00
+        self._snr = 0.00
+
+    @property
+    def input(self):
+        return self._input
+
+    @property
+    def output(self):
+        return self._output
+
+    @property
+    def signal_power(self):
+        return self._signal_power
+
+    @property
+    def latency(self):
+        return self._latency
+
+    @property
+    def snr(self):
+        return self._snr
+
+    @latency.setter
+    def latency(self, latency):
+        self._latency = latency
+
+    @snr.setter
+    def snr(self, snr):
+        self._snr = snr
+
 
 if __name__ == '__main__':
     network = Network('nodes.json')
