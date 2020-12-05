c7a2ce686107a26f7d27cf6279324ee819f83bff
Salvo <s267560@studenti.polito.it>
2020-12-05 16:29:55 +0100

    Implemented main method and tested

--- lab3/elements.py
+++ lab3/elements.py
@@ -3,6 +3,7 @@ from scipy.constants import c
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class Node(object):
     def __init__(self, node_dictionary):
@@ -160,3 +161,40 @@ class Network(object):
                 plt.plot([x0, x1], [y0, y1], 'b')
         plt.title('Network')
         plt.show()
+
+
+if __name__ == '__main__':
+    network = Network('nodes.json')
+    network.connect()
+    node_labels = network.nodes.keys()
+    pairs = []
+    for label1 in node_labels:
+        for label2 in node_labels:
+
+            if label1 != label2:
+                pairs.append(label1 + label2)
+            columns = ['path', 'latency', 'noise', 'snr']
+            df = pd.DataFrame()
+            paths = []
+            latencies = []
+            noises = []
+            snrs = []
+            for pair in pairs:
+                for path in network.find_paths(pair[0], pair[1]):
+                    path_string = ''
+                    for node in path:
+                        path_string += node + '->'
+                    paths.append(path_string[:-2])
+                    signal_information = Signal_information(1e-3, path)
+                    signal_information = network.propagate(signal_information)
+                    latencies.append(signal_information.latency)
+                    noises.append(signal_information.noise_power)
+                    snrs.append(
+                        10 * np.log10(
+                            signal_information.signal_power / signal_information.noise_power))
+            df['path'] = paths
+            df['latency'] = latencies
+            df['noise'] = noises
+            df['snr'] = snrs
+
+    network.draw()
