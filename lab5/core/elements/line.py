2ab739eeaf19f885774c849d671985aa43528f21
Salvo <s267560@studenti.polito.it>
2020-12-06 17:03:19 +0100

    Updated state attribute in class Line
    lab 5.2

--- lab5/core/elements/line.py
+++ lab5/core/elements/line.py
@@ -1,4 +1,6 @@
 from scipy.constants import c
+from lab5.core.info.signalInformation import SignalInformation
+from lab5.core.info.lightpath import Lightpath
 
 
 class Line(object):
@@ -6,7 +8,9 @@ class Line(object):
         self._label = line_dictionary['label']
         self._length = line_dictionary['length']
         self._successive = {}
-        self._state = 'free'
+        self._state = []
+        for i in range(10):
+            self._state.append(None)
 
     @property
     def label(self):
@@ -40,7 +44,7 @@ class Line(object):
         noise = 1e-9 * signal_power * self.length
         return noise
 
-    def propagate(self, signal_information):
+    def propagate(self, signal_information: SignalInformation):
         signal_power = signal_information.signal_power
         noise = self.noise_generation(signal_power)
         signal_information.add_noise(noise)
@@ -48,4 +52,16 @@ class Line(object):
         signal_information.add_latency(latency)
         node = self.successive[signal_information.path[0]]
         signal_information = node.propagate(signal_information)
+        return signal_information
+
+    def propagate(self, lightpath: Lightpath):
+        if lightpath.channel is not None:
+            self.state.insert(lightpath.channel, 'occupied')
+        signal_power = lightpath.signal_power
+        noise = self.noise_generation(signal_power)
+        lightpath.add_noise(noise)
+        latency = self.latency_generation()
+        lightpath.add_latency(latency)
+        node = self.successive[lightpath.path[0]]
+        signal_information = node.propagate(lightpath)
         return signal_information
